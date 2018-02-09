d467d75ce490aa36080b01bc25e73f63a98e964a
Holger Haas <h.r.o.haas@gmail.com>
2018-02-09 18:25:19 +0100

    Cleanup lang.ObjectInterface and lang.Interface

--- src/lang/Interface.js
+++ src/lang/Interface.js
@@ -1,121 +1,41 @@
-JARS.module('lang.Interface').$import([{
-    System: ['Modules::getCurrentModuleData', 'Logger', '::isFunction', '::isNumber']
-}, '.Class', '.Array!Check,Derive,Iterate']).$export(function(getCurrentModuleData, Logger, isFunction, isNumber, Class, Arr) {
+JARS.module('lang.Interface').$import(['.ObjectInterface', '.Array.Derive::filter', {
+    '.Class': ['.', '::isClass', '::isInstance']
+}]).$export(function(ObjectInterface, filter, Class, isClass, isInstance) {
     'use strict';
 
-    var IMPLEMENTED_METHODS_MISSING = '${impl} must implement the method(s): "${missingMethods}" !',
-        IMPLEMENTOR_MISSING = 'No Class, instance or Object given to check!',
-        IMPLEMENTORTYPE_MISMATCH = 'You must provide a Class or instance to check',
+    var MSG_IMPLEMENTOR_TYPE_MISMATCH = 'You must provide a class or instance to check',
+        MSG_INTERFACE_MISSING = 'There is no interface given to compare with!',
         Interface;
 
     Interface = Class('Interface', {
-        _$: {
-            name: '',
-
-            methods: null,
-
-            logger: null,
-
-            addMethod: function(method) {
-                var methods = this._$methods;
-
-                methods.some(checkMethodExists, method) || methods.push(method);
-            }
+        construct: function(interfaceName, methods) {
+            this.$super(interfaceName, methods);
         },
 
-        $: {
-            construct: function(interfaceName, methods) {
-                this._$name = interfaceName;
-                this._$methods = Arr.from(methods);
-                this._$logger = new Logger('Interface "#<' + getCurrentModuleData().moduleName + ':' + interfaceName + '>"');
-            },
+        _$: {
+            hasImplementor: function(implementor) {
+                var hasImplementor = this.$super(implementor);
 
-            extendz: function(superInterface) {
-                var iface = this;
+                if(hasImplementor) {
+                    hasImplementor = isClass(implementor) ? !!implementor.prototype : isInstance(implementor) ? !!implementor : null;
 
-                if (Interface.isInstance(superInterface)) {
-                    iface.$proxy(superInterface, proxiedGetMethods).each(iface._$addMethod, iface);
-                }
-
-                return iface;
-            },
-
-            getName: function() {
-                return this._$name;
-            },
-
-            isImplementedBy: function(implementor, checkAny) {
-                var logger = this._$logger,
-                    methods = this._$methods,
-                    isImplemented = false,
-                    isImplementorClass = Class.isClass(implementor),
-                    isImplementorInstance = !isImplementorClass && Class.isInstance(implementor),
-                    objectToCheck, notImplementedMethods;
-
-                if (implementor) {
-                    objectToCheck = isImplementorClass ? implementor.prototype : (checkAny || isImplementorInstance) ? implementor : null;
-
-                    if (objectToCheck) {
-                        notImplementedMethods = methods.filter(isMethodNotImplemented, objectToCheck);
-
-                        if (notImplementedMethods.length) {
-                            logger.error(IMPLEMENTED_METHODS_MISSING, {
-                                impl: (isImplementorClass || isImplementorInstance) ? implementor.getHash() : implementor,
-
-                                missingMethods: notImplementedMethods.map(transformMethodData).join('", "')
-                            });
-                        }
-                        else {
-                            isImplemented = true;
-                        }
-                    }
-                    else {
-                        logger.warn(IMPLEMENTORTYPE_MISMATCH);
+                    if(!hasImplementor) {
+                        this._$logger.warn(MSG_IMPLEMENTOR_TYPE_MISMATCH);
                     }
                 }
-                else {
-                    logger.warn(IMPLEMENTOR_MISSING);
-                }
 
-                return isImplemented;
+                return hasImplementor;
+            },
+
+            findNotImplemented: function(implementor) {
+                return this.$super(isClass(implementor) ? implementor.prototype : implementor);
+            },
+
+            logNotImplemented: function(implementor, notImplementedMethods) {
+                this.$super((isClass(implementor) || isInstance(implementor)) ? implementor.getHash() : implementor, notImplementedMethods);
             }
         }
-    }, {
-        isImplementedBy: function(interfaces, implementor, checkAny) {
-            return Arr.every(interfaces, implementzInterface, {
-                impl: implementor,
-
-                any: checkAny
-            });
-        }
-    });
-
-    function proxiedGetMethods() {
-        /*jslint validthis: true */
-        return this._$methods;
-    }
-
-    function checkMethodExists(method) {
-        /*jslint validthis: true */
-        return method[0] === this[0];
-    }
-
-    function isMethodNotImplemented(methodData) {
-        /*jslint validthis: true */
-        var methodToCheck = this[methodData[0]],
-            args = methodData[1];
-
-        return !isFunction(methodToCheck) || (isNumber(args) && !(args === methodToCheck.length || args === methodToCheck.arity));
-    }
-
-    function transformMethodData(methodData) {
-        return methodData.join(' (arguments: ') + ')';
-    }
-
-    function implementzInterface(iface) {
-        /*jslint validthis: true */
-        return iface.isImplementedBy(this.impl, this.any);
-    }
+    }).extendz(ObjectInterface);
 
     /**
      * Checks whether any method of InterFace.methods is defined in the Class
@@ -125,18 +45,16 @@ JARS.module('lang.Interface').$import([{
      *
      * @return Object
      */
-    Class.addStatic('implementz', function() {
-        /*jslint validthis: true */
+    Class.addStaticMethod('implementz', function() {
         var isImplemented = false,
             currentClass = this,
-
-            interfaces = Arr.filter(arguments, Interface.isInstance, Interface);
+            interfaces = filter(arguments, Interface.isInstance, Interface);
 
         if (interfaces.length) {
             isImplemented = Interface.isImplementedBy(interfaces, currentClass);
         }
         else {
-            currentClass.logger.warn('There is no interface given to compare with!');
+            currentClass.logger.warn(MSG_INTERFACE_MISSING);
         }
 
         return isImplemented && currentClass;
--- src/lang/ObjectInterface.js
+++ src/lang/ObjectInterface.js
@@ -1,41 +1,31 @@
 JARS.module('lang.ObjectInterface').$import([{
-    System: ['Modules::getCurrentModuleData', 'Logger', '::isFunction', '::isNumber']
-}, '.Class', '.Array!Check,Derive,Iterate']).$export(function(getCurrentModuleData, Logger, isFunction, isNumber, Class, Arr) {
+    System: ['Modules::getCurrentModuleData', 'Logger', '::isFunction', '::isNumber'],
+    '.Array': ['Iterate::each', {
+        Check: ['::some', '::every'],
+        Derive: ['::filter', '::map']
+    }]
+}, '.Class']).$export(function(getCurrentModuleData, Logger, isFunction, isNumber, each, some, every, filter, map, Class) {
     'use strict';
 
-	// TODO
-    var IMPLEMENTED_METHODS_MISSING = '${impl} must implement the method(s): "${missingMethods}" !',
-        IMPLEMENTOR_MISSING = 'No Class, instance or Object given to check!',
-        IMPLEMENTORTYPE_MISMATCH = 'You must provide a Class or instance to check',
+    var MSG_IMPLEMENTED_METHODS_MISSING = '${implementor} must implement the method(s): "${missingMethods}" !',
+        MSG_IMPLEMENTOR_MISSING = 'No implementor given to check!',
         ObjectInterface;
 
     ObjectInterface = Class('ObjectInterface', {
-        _$: {
-            name: '',
-
-            methods: null,
-
-            logger: null,
-
-            addMethod: function(method) {
-                var methods = this._$methods;
-
-                methods.some(checkMethodExists, method) || methods.push(method);
-            }
-        },
-
         $: {
             construct: function(interfaceName, methods) {
-                this._$name = interfaceName;
-                this._$methods = Arr.from(methods);
-                this._$logger = new Logger('Interface "#<' + getCurrentModuleData().moduleName + ':' + interfaceName + '>"');
+                this._$name = this.Class.getClassName() + ' #<' + getCurrentModuleData().moduleName + ':' + interfaceName + '>';
+                this._$methods = methods || [];
+                this._$logger = new Logger(this._$name);
             },
 
             extendz: function(superInterface) {
                 var iface = this;
 
-                if (ObjectInterface.isInstance(superInterface)) {
-                    iface.$proxy(superInterface, proxiedGetMethods).each(iface._$addMethod, iface);
+                if (iface.Class.isInstance(superInterface)) {
+                    each(iface.$proxy(superInterface, function() {
+                        return this._$methods;
+                    }), iface._$addMethod, iface);
                 }
 
                 return iface;
@@ -46,68 +36,78 @@ JARS.module('lang.ObjectInterface').$import([{
             },
 
             isImplementedBy: function(implementor) {
-                var logger = this._$logger,
-                    methods = this._$methods,
-                    isImplemented = false,
+                var isImplemented = false,
                     notImplementedMethods;
 
-                if (implementor) {
-                    notImplementedMethods = methods.filter(isMethodNotImplemented, implementor);
+                if (this._$hasImplementor(implementor)) {
+                    notImplementedMethods = this._$findNotImplemented(implementor);
 
                     if (notImplementedMethods.length) {
-                        logger.error(IMPLEMENTED_METHODS_MISSING, {
-                            impl: implementor,
-
-                            missingMethods: notImplementedMethods.map(transformMethodData).join('", "')
-                        });
+                        this._$logNotImplemented(implementor, notImplementedMethods);
                     }
                     else {
                         isImplemented = true;
                     }
                 }
-                else {
-                    logger.warn(IMPLEMENTOR_MISSING);
-                }
 
                 return isImplemented;
             }
+        },
+
+        _$: {
+            name: '',
+
+            methods: null,
+
+            logger: null,
+
+            addMethod: function(newMethod) {
+                var methods = this._$methods;
+
+                some(methods, function(method) {
+                    return method[0] === newMethod[0];
+                }) || methods.push(newMethod);
+            },
+
+            hasImplementor: function(implementor) {
+                var hasImplementor = false;
+
+                if(implementor) {
+                    hasImplementor = true;
+                }
+                else {
+                    this._$logger.warn(MSG_IMPLEMENTOR_MISSING);
+                }
+
+                return hasImplementor;
+            },
+
+            findNotImplemented: function(implementor) {
+                return filter(this._$methods, function(methodData) {
+                    var methodToCheck = implementor[methodData[0]],
+                        arity = methodData[1];
+
+                    return !isFunction(methodToCheck) || (isNumber(arity) && !(arity === methodToCheck.length || arity === methodToCheck.arity));
+                });
+            },
+
+            logNotImplemented: function(implementor, notImplementedMethods) {
+                this._$logger.error(MSG_IMPLEMENTED_METHODS_MISSING, {
+                    implementor: implementor,
+
+                    missingMethods: map(notImplementedMethods, function(methodData) {
+                        return methodData.join(' (arguments: ') + ')';
+                    }).join('", "')
+                });
+            }
         }
     }, {
-        isImplementedBy: function(interfaces, implementor, checkAny) {
-            return Arr.every(interfaces, implementzInterface, {
-                impl: implementor,
-
-                any: checkAny
+        isImplementedBy: function(interfaces, implementor) {
+            return every(interfaces, function(iface) {
+                return iface.isImplementedBy(implementor);
             });
         }
     });
-
-    function proxiedGetMethods() {
-        /*jslint validthis: true */
-        return this._$methods;
-    }
-
-    function checkMethodExists(method) {
-        /*jslint validthis: true */
-        return method[0] === this[0];
-    }
-
-    function isMethodNotImplemented(methodData) {
-        /*jslint validthis: true */
-        var methodToCheck = this[methodData[0]],
-            args = methodData[1];
-
-        return !isFunction(methodToCheck) || (isNumber(args) && !(args === methodToCheck.length || args === methodToCheck.arity));
-    }
-
-    function transformMethodData(methodData) {
-        return methodData.join(' (arguments: ') + ')';
-    }
-
-    function implementzInterface(iface) {
-        /*jslint validthis: true */
-        return iface.isImplementedBy(this.impl, this.any);
-    }
 
     return ObjectInterface;
 });
